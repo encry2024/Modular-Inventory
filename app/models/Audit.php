42c6402b17b42639d2f0cf2b9754293dce6845ae
Jake <christan.jake2025@yahoo.com>
2014-10-22 22:02:33 +0800

    adding paginator

--- app/models/Audit.php
+++ app/models/Audit.php
@@ -5,7 +5,7 @@ class Audit extends Eloquent {
 
 	public static function getAudits() {
 		$audits = Audit::distinct('history', 'created')->orderBy('created_at', 'desc')->get();
-
+		
 		return View::make('trackall')
 				->with('audits', $audits);
 	}
